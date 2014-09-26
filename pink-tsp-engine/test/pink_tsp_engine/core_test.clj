aaec2b6a3b707ad300c66981194ed61b25746380
Marcin Nowakowski <marcin.nowakowski@gazeta.pl>
2014-09-26 15:06:30 +1200

    Creating a tsp engine project without content (hello world)

---
+++ pink-tsp-engine/test/pink_tsp_engine/core_test.clj
@@ -0,0 +1,7 @@
+(ns pink-tsp-engine.core-test
+  (:require [clojure.test :refer :all]
+            [pink-tsp-engine.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
